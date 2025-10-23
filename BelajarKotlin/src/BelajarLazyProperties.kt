bfeab841c898dfc59aaf5a674906adbaa0bd9a5a
2226250050anJovansa <jovansaputralaksana_2226250050@mhs.mdp.ac.id>
2025-10-23 20:37:33 +0700

    1

---
+++ BelajarKotlin/src/BelajarLazyProperties.kt
@@ -0,0 +1,15 @@
+class TranskripNilai {
+    val list: List<Char> by lazy {
+        println("List di akses")
+        listOf('A', 'B', 'C')
+    }
+}
+
+fun main() {
+    val nilaiMhs = TranskripNilai()
+    println(nilaiMhs.list)
+    println(nilaiMhs.list)
+    println(nilaiMhs.list)
+    println(nilaiMhs.list)
+
+}
